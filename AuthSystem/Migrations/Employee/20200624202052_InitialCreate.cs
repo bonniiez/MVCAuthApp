33b4ad5502231e2081088de9d50d249fcadedb30
Bonnie <bonniezhengg@alumni.ubc.ca>
2020-06-24 14:07:07 -0700

    created migration to sync with new dbcontext change

---
+++ AuthSystem/Migrations/Employee/20200624202052_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace AuthSystem.Migrations.Employee
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    EmployeeCode = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
