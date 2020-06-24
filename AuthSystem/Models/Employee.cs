ee27a60542f55c37dbed314a807d5161037bcd8b
Bonnie <bonniezhengg@alumni.ubc.ca>
2020-06-24 14:05:11 -0700

    created Employee/EmployeeContext classes

---
+++ AuthSystem/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthSystem.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string EmployeeCode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string OfficeLocation { get; set; }
+
+    }
+}
